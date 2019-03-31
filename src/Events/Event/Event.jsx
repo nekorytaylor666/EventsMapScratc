20ae28fe982697345644ebbbea1dc2f54993dbee
Тохтар <art_gulba@mail.ru>
2019-03-31 19:38:22 +0900

    .

--- src/Events/Event/Event.jsx
+++ src/Events/Event/Event.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Event = (props) => {
-    const eventDate = new Date(props.time)
-  return (
-    <div>
-        <div className="bg-white br3 ba b--black-10 shadow-5 flex flex-column justify-around js-content">
-            <h1>{props.title}</h1><br/>
-            <p>{props.desc}</p><br/>
-            <p>---- {props.address}</p>
-            
-        </div>
-    </div>
-  )
-}
+  // FIX eventdate not working, said this is an object
+  const eventDate = new Date(props.time);
+	return (
+		<div>
+			<div className="bg-white br3 ba b--black-10 shadow-5 flex flex-column justify-around js-content">
+				<h1>{props.title}</h1>
+				<br />
+				<p>{props.desc}</p>
+				<br />
+				<p>---- {props.address}</p>
+			</div>
+		</div>
+	);
+};
 
-export default Event
+export default Event;
